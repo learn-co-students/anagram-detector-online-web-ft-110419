a2920c14a92fb653b8bfd05a06cb12a8750286f4
Kevin Downs <kevin.downs04@gmail.com>
2019-11-14 15:35:33 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,8 +10,11 @@ class Anagram
   def match(poss_anagrams)
     sorted_word = @word.chars.sort.join
     sorted_list = poss_anagrams.map {|word| word.chars.sort.join}
-    index = sorted_list.find_index{|sorted| sorted == sorted_word}
-    index ? Array.new(poss_anagrams[index]) : []
+    results = []
+    sorted_list.each_with_index do |sorted, index|
+      results << poss_anagrams[index] if sorted_word == sorted
+    end
+    results
   end
 
 end
