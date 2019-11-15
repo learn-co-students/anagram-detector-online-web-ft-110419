dd40afe4f31f153ed5d304ab912098b94b8e508a
Jess F <robotspacefish@gmail.com>
2019-11-15 14:16:09 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    anagrams = possible_anagrams.find_all do |w|
+      a = w.split("").sort.join("")
+      b = @word.split("").sort.join("")
+      a == b
+    end
+  end
+end
