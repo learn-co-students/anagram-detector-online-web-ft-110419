eb4de899942e2d8a7c2f05151132f226b41c6912
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-17 22:57:35 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+# #Learn parameters
+# should detect no matches
+# should detect a simple anagram
+# should detect an anagram
+# should detect multiple anagrams
+class Anagram
+  attr_accessor :word, :words
+  def initialize(word)
+    @word = word
+  end
+  def match(words)
+    words.select {|w| w.split("").sort == word.split("").sort}
+  end
+end
