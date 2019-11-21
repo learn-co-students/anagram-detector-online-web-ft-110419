6c14ba3692b1ba72258eaabd67f1791528e8196a
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-21 23:00:12 +0000

    Done.

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,12 +1,12 @@
-describe 'Anagram' do 
+describe 'Anagram' do
   it 'should detect no matches' do
     diaper = Anagram.new('diaper')
     expect(diaper.match(%w(hello world zombies pants dipper))).to eq([])
-  end
+  end #%w is being used here in a paranthesis with no quotation marks
 
   it 'should detect a simple anagram' do
     detector = Anagram.new('ba')
-    ba = detector.match(['ab', 'abc', 'bac'])
+    ba = detector.match(['ab', 'abc', 'bac']) #this is already an array
     expect(ba).to eq(['ab'])
   end
 
@@ -14,11 +14,11 @@ describe 'Anagram' do
     detector = Anagram.new('listen')
     listen = detector.match %w(enlists google inlets banana)
     expect(listen).to eq(['inlets'])
-  end
+  end #%w is being used here
 
   it 'should detect multiple anagrams' do
     detector = Anagram.new('allergy')
     allergy = detector.match %w(gallery ballerina regally clergy largely leading)
     expect(allergy).to eq(['gallery', 'regally', 'largely'])
   end
-end
+end #it is expecting the unseparated unquoted data to be turned in to an array
