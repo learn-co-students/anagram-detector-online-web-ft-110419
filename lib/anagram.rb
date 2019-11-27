30d53a106e882d77ac043031ccb84100d1aa31d6
Sean Good <tree662@protonmail.com>
2019-11-27 07:34:03 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
+class Anagram
+  
+  attr_accessor :anagram 
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def match(words)
+    words.select do |word|
+      is_anagram?(word)
+    end
+  end
+
+  def is_anagram?(word)
+    word.chars.sort == @anagram.chars.sort
+  end
+end
+
 # Your code goes here!
